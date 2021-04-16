32f46e18d903087416d8044f9dc8c389ab62ca5e
Marcos Ribeiro Hioka <marcosribeirohioka@MacBook-Pro-de-Marcos.local>
2021-04-16 18:59:41 -0300

    First commit

---
+++ performance_test.js
@@ -0,0 +1,21 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+export let options = {
+  duration: '1m',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+  },
+};
+
+export default function () {
+  http.get('http://test.k6.io/contacts.php');
+  sleep(3);
+}
+
+export function handleSummary(data) {
+    console.log('Preparing the end-of-test summary...');
+    return {
+        'report/summary.json': JSON.stringify(data),
+    }
+  }
